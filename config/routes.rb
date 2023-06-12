4ef2d72e655ffaffd9ccbb409d2d2e080f293d01
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-12 14:09:11 +0300

    Configure Api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     get 'messages/random', to: 'messages#random_greeting'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
