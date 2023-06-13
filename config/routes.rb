fe790808a26274474b919ef98449d557e9437bc2
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-13 14:19:32 +0300

    Create static root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
   namespace :api do
     get 'messages/random', to: 'messages#random_greeting'
   end
   # Defines the root path route ("/")
-  # root "articles#index"
 end
